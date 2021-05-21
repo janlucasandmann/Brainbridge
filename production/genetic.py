b86ab76e485b9ec899a446831a06923b7f77ad69
janlucasandmann <janls2601@icloud.com>
2021-05-21 17:58:36 +0200

    [BRAIN_21] Production: Add Genetic Algorithm

---
+++ production/genetic.py
@@ -0,0 +1,123 @@
+""" Genetic algorithm for feature selection """
+
+import random
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+import numpy as np
+
+def generateDnaString(numberOfGenes, X_input):
+    # X_input must be transposed!
+
+    possibleGenes = len(X_input)
+    geneNumbers = []
+    dnaString = []
+
+    i = 0
+    while i < numberOfGenes:
+        rnd = -1
+        while rnd in geneNumbers or rnd == -1:
+            rnd = random.randint(0, (possibleGenes - 1))
+
+        geneNumbers.append(rnd)
+        dnaString.append(X_input[rnd])
+
+        i += 1
+
+    return [geneNumbers, np.transpose(dnaString)]
+
+def evaluateDnaString(dnaString, events, trainingNumber, estimators):
+    # Create and train random forest model
+
+    clf=RandomForestClassifier(n_estimators=estimators)
+    clf.fit(dnaString[:trainingNumber - 1], events[:trainingNumber - 1])
+
+    prediction=clf.predict(dnaString[trainingNumber:])
+
+    return metrics.accuracy_score(events[trainingNumber:], prediction)
+
+def killWeakSubjects(dnaStrings, killRate, trainingNumber, estimators):
+
+    kill = len(dnaStrings) * killRate
+    res = [] 
+
+    for i in dnaStrings:
+        res.append(evaluateDnaString(i[1], events, trainingNumber, estimators))
+
+    sortedDnaStrings = [x for _,x in sorted(zip(res,dnaStrings))]
+
+    return sortedDnaStrings[len(sortedDnaStrings) - int(kill) + 1:]
+
+def sex(subjectOne, subjectTwo, numberOfKids, X_input, numberOfMutations):
+    # X_input must be transposed!
+
+    possibleGenes = len(X_input)
+    kids = []
+    i = 0
+
+    while i < numberOfKids:
+        kidDnaString = []
+        kidGeneNumbers = []
+
+        c = 0
+        while c < len(subjectOne[0]):
+            rnd = random.randint(0,1)
+
+            if rnd == 0:
+                kidDnaString.append(np.transpose(subjectOne[1])[c])
+                kidGeneNumbers.append(subjectOne[0][c])
+            else:
+                kidDnaString.append(np.transpose(subjectTwo[1])[c])
+                kidGeneNumbers.append(subjectTwo[0][c])
+
+            c += 1
+
+        kids.append(mutateGenes(kidGeneNumbers, kidDnaString, numberOfMutations, X_input))
+
+        i += 1
+
+    return kids
+
+def mutateGenes(geneNumbers, dnaString, numberOfMutations, X_input):
+    # X_input must be transposed!
+
+
+    possibleGenes = len(X_input)
+
+    i = 0
+    while i < numberOfMutations:
+        geneNumber = random.randint(0, len(geneNumbers) - 1)
+        rnd = random.randint(0, possibleGenes - 1)
+
+        geneNumbers[geneNumber] = rnd
+        dnaString[geneNumber] = X_input[rnd]
+
+        i += 1
+
+    return [geneNumbers, np.transpose(dnaString)]
+
+def simulateEvolution(X_input, numberOfGenes, numberOfSubjects, numberOfGenerations, numberOfMutations, killRate, trainingNumber, estimators):
+    # X_input must be transposed!
+
+    subjects = []
+
+    i = 0
+    while i < numberOfSubjects:
+        subjects.append(generateDnaString(numberOfGenes, X_input))
+        i += 1
+
+    k = 0
+    while k < numberOfGenerations:
+        decimatedSubjects = killWeakSubjects(subjects, killRate, trainingNumber, estimators)
+        kids = []
+        subjects = decimatedSubjects
+
+        c = 0
+        while c < len(decimatedSubjects[0]):
+            new_kids = sex(decimatedSubjects[c], decimatedSubjects[len(decimatedSubjects) - c - 1], 2, X_input, numberOfMutations)
+            
+            for i in new_kids:
+                subjects.append(i)
+            c += 1
+        k += 1
+
+    return subjects
